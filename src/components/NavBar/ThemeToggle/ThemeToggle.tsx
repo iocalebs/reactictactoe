27e6ded6b972b7aff54125173e53d44a1a44b73f
Caleb St-Denis <caleb.stdenis@gmail.com>
2023-09-27 11:35:07 -0400

    Set theme toggle to retain focus on keyboard entry

--- src/components/NavBar/ThemeToggle/ThemeToggle.tsx
+++ src/components/NavBar/ThemeToggle/ThemeToggle.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { IconMoon, IconSun } from "@/components/Icon";
+import { useRef } from "react";
 
 declare global {
 	interface Window {
@@ -9,19 +10,30 @@ declare global {
 }
 
 export function ThemeToggle() {
+	const sunRef = useRef<HTMLButtonElement>(0);
+	const moonRef = useRef<HTMLButtonElement>(0);
+
 	return (
 		<>
 			<button
-				className="nav-button dark:hidden"
-				onClick={() => window.__setTheme("dark")}
-			>
-				<IconMoon />
-			</button>
-			<button
+				ref={sunRef}
 				className="nav-button hidden dark:flex"
-				onClick={() => window.__setTheme("light")}
+				onClick={() => {
+					window.__setTheme("light");
+					moonRef.current.focus();
+				}}
 			>
 				<IconSun />
+			</button>
+			<button
+				ref={moonRef}
+				className="nav-button dark:hidden"
+				onClick={() => {
+					window.__setTheme("dark");
+					sunRef.current.focus();
+				}}
+			>
+				<IconMoon />
 			</button>
 		</>
 	);
