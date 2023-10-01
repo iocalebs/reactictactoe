3d7a45684cd361304b5ddf9623e1dc4a3c8efcd0
Caleb St-Denis <caleb.stdenis@gmail.com>
2023-10-01 19:47:04 -0400

    Add pulse animation to reset button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps {
 export function Button({ children, icon, onClick }: ButtonProps) {
 	return (
 		<button
-			className="flex items-center gap-1 rounded bg-blue-600 py-2 pl-2 pr-3 text-neutral-100 drop-shadow-md transition-colors hover:bg-blue-700"
+			className="flex animate-pulse items-center gap-1 rounded bg-blue-600 py-2 pl-2 pr-3 text-neutral-100 drop-shadow-md transition-colors hover:animate-none hover:bg-blue-700"
 			onClick={onClick}
 		>
 			<div className="flex-grow-0 justify-self-start">{icon}</div>
